b51de5ad40a2fa554819d3dd5772307789719e5c
SE06436 <SE06436@S4U43748.bankinter.bk>
2018-08-13 12:16:11 +0200

    Se quitan imports

--- src/main/java/net/bounceme/chronos/comunicacion/controllers/AvisosController.java
+++ src/main/java/net/bounceme/chronos/comunicacion/controllers/AvisosController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.ApiOperation;
 import net.bounceme.chronos.comunicacion.controllers.params.ParamsAviso;
 import net.bounceme.chronos.comunicacion.exceptions.ControllerException;
 import net.bounceme.chronos.comunicacion.exceptions.ServiceException;

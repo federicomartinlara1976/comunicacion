f01d43bd3219b8118a3c2fd8b65113a5e5363cd2
SE06436 <SE06436@S4U43748.bankinter.bk>
2018-08-22 10:12:04 +0200

    Se parametrizan cadenas de copyright para Swagger

--- src/main/java/net/bounceme/chronos/comunicacion/swagger/SwaggerConfig.java
+++ src/main/java/net/bounceme/chronos/comunicacion/swagger/SwaggerConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,19 +17,46 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
-	public static final Contact DEFAULT_CONTACT = new Contact("Federico Martin Lara", "http://www.github.com/federicomartinlara1976/comunicacion",
-			"federicomartinlara1976@gmail.com");
-
-	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Comunicacion", "Prueba de API para Tinsa", "1.0",
-			"urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
-
+	
 	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
 			Arrays.asList("application/json", "application/xml"));
+	
+	@Value("${swagger.contact.name}")
+	private String contactName;
+	
+	@Value("${swagger.contact.web}")
+	private String contactWeb;
+	
+	@Value("${swagger.contact.email}")
+	private String contactEmail;
+	
+	@Value("${swagger.app.name}")
+	private String appName;
+	
+	@Value("${swagger.app.description}")
+	private String appDesc;
+	
+	@Value("${swagger.app.version}")
+	private String appVersion;
+	
+	@Value("${swagger.app.license}")
+	private String appLicense;
+	
+	@Value("${swagger.app.urlLicense}")
+	private String appUrlLicense;
+	
+	private Contact defaultContact() {
+		return new Contact(contactName, contactWeb, contactEmail);
+	}
+	
+	private ApiInfo defaultApiInfo() {
+		return new ApiInfo(appName, appDesc, appVersion,
+				"urn:tos", defaultContact(), appLicense, appUrlLicense);
+	}
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES)
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(defaultApiInfo()).produces(DEFAULT_PRODUCES_AND_CONSUMES)
 				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 }

7bede5f3ffd63a9b57854e6f4b1f7c94aedfd597
SE06436 <SE06436@S4U43748.bankinter.bk>
2019-01-04 14:53:13 +0100

    Fixed assembler

--- src/main/java/net/bounceme/chronos/comunicacion/services/assemblers/MedioComunicacionClienteAssembler.java
+++ src/main/java/net/bounceme/chronos/comunicacion/services/assemblers/MedioComunicacionClienteAssembler.java
@@ -1,10 +1,14 @@
 package net.bounceme.chronos.comunicacion.services.assemblers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import net.bounceme.chronos.comunicacion.dto.MedioComunicacionClienteDTO;
+import net.bounceme.chronos.comunicacion.dto.TipoComunicacionDTO;
 import net.bounceme.chronos.comunicacion.model.MedioComunicacionCliente;
+import net.bounceme.chronos.comunicacion.model.TipoComunicacion;
+import net.bounceme.chronos.utils.assemblers.BidirectionalAssembler;
 import net.bounceme.chronos.utils.assemblers.BidirectionalGenericAssembler;
 import net.bounceme.chronos.utils.exceptions.AssembleException;
 
@@ -12,7 +16,8 @@ import net.bounceme.chronos.utils.exceptions.AssembleException;
 public class MedioComunicacionClienteAssembler extends BidirectionalGenericAssembler<MedioComunicacionCliente, MedioComunicacionClienteDTO> {
 
     @Autowired
-    private TipoComunicacionAssembler tipoComunicacionAssembler;
+    @Qualifier("tipoComunicacionAssembler")
+    private BidirectionalAssembler<TipoComunicacion, TipoComunicacionDTO> tipoComunicacionAssembler;
     
     public MedioComunicacionClienteAssembler() {
         super(MedioComunicacionCliente.class, MedioComunicacionClienteDTO.class);

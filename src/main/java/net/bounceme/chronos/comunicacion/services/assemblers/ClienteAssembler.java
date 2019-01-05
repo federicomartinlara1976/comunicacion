ad38d9883088c3db3b3f9527c002446a9aa4377a
Federico Martín Lara <federicomartinlara1976@gmail.com>
2019-01-05 13:20:44 +0100

    Fix assembler

--- src/main/java/net/bounceme/chronos/comunicacion/services/assemblers/ClienteAssembler.java
+++ src/main/java/net/bounceme/chronos/comunicacion/services/assemblers/ClienteAssembler.java
@@ -1,14 +1,32 @@
 package net.bounceme.chronos.comunicacion.services.assemblers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import net.bounceme.chronos.comunicacion.dto.ClienteDTO;
+import net.bounceme.chronos.comunicacion.dto.DireccionClienteDTO;
+import net.bounceme.chronos.comunicacion.dto.MedioComunicacionClienteDTO;
 import net.bounceme.chronos.comunicacion.model.Cliente;
+import net.bounceme.chronos.comunicacion.model.DireccionCliente;
+import net.bounceme.chronos.comunicacion.model.MedioComunicacionCliente;
+import net.bounceme.chronos.utils.assemblers.Assembler;
 import net.bounceme.chronos.utils.assemblers.BidirectionalGenericAssembler;
 import net.bounceme.chronos.utils.exceptions.AssembleException;
 
 @Component("clienteAssembler")
 public class ClienteAssembler extends BidirectionalGenericAssembler<Cliente, ClienteDTO> {
+	
+	@Autowired
+	@Qualifier("medioComunicacionClienteAssembler")
+	private Assembler<MedioComunicacionCliente, MedioComunicacionClienteDTO> mediosComunicacionAssembler;
+	
+	@Autowired
+	@Qualifier("direccionClienteAssembler")
+	private Assembler<DireccionCliente, DireccionClienteDTO> direccionesClienteAssembler;
 
     public ClienteAssembler() {
         super(Cliente.class, ClienteDTO.class);
@@ -34,6 +52,12 @@ public class ClienteAssembler extends BidirectionalGenericAssembler<Cliente, Cli
         clienteDTO.setNombre(source.getNombre());
         clienteDTO.setApellidos(source.getApellidos());
         clienteDTO.setDni(source.getDni());
+        
+        List<MedioComunicacionClienteDTO> listMediosComunicacion = new ArrayList<>(mediosComunicacionAssembler.assemble(source.getMediosComunicacion()));
+        List<DireccionClienteDTO> listDirecciones = new ArrayList<>(direccionesClienteAssembler.assemble(source.getDirecciones()));
+        
+        clienteDTO.setMediosComunicacion(listMediosComunicacion);
+        clienteDTO.setDirecciones(listDirecciones);
         
         return clienteDTO;
     }

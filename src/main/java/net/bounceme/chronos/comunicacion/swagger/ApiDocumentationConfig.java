179026f3f46ce9773b82d9231caccac5e30718ac
Federico Martín Lara <federicomartinlara1976@gmail.com>
2019-01-05 21:03:31 +0100

    Release 1.2.0

--- src/main/java/net/bounceme/chronos/comunicacion/swagger/ApiDocumentationConfig.java
+++ src/main/java/net/bounceme/chronos/comunicacion/swagger/ApiDocumentationConfig.java
@@ -9,7 +9,7 @@ import io.swagger.annotations.SwaggerDefinition;
 @SwaggerDefinition(
         info = @Info(
                 description = "Prueba de API para Tinsa",
-                version = "1.1.9-RELEASE",
+                version = "1.2.0-RELEASE",
                 title = "Comunicacion",
                 contact = @Contact(
                    name = "Federico Martín Lara", 

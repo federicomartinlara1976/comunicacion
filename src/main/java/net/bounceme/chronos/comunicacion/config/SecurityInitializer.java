61bced9db20d728f8ac2c51c1e9fdbd42affe703
Federico Martín Lara <federico@horizontefinal>
2024-02-22 11:16:17 +0100

    Fixed bugs

--- src/main/java/net/bounceme/chronos/comunicacion/config/SecurityInitializer.java
+++
@@ -1,5 +0,0 @@
-package net.bounceme.chronos.comunicacion.config;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-} 

38231af3178d5cb7faf0a3998823b3f5310a42d5
Federico Martín Lara <federico@horizontefinal>
2024-03-21 10:59:43 +0100

    Queue config separate

--- src/main/java/net/bounceme/chronos/comunicacion/config/AppConfig.java
+++ src/main/java/net/bounceme/chronos/comunicacion/config/AppConfig.java
@@ -2,14 +2,6 @@ package net.bounceme.chronos.comunicacion.config;
 
 import java.text.SimpleDateFormat;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -19,45 +11,9 @@ import net.bounceme.chronos.comunicacion.utils.Constantes;
 @Configuration
 public class AppConfig {
 	
-	@Value("${application.queue}")
-	private String queueName;
-	
-	@Value("${application.topic}")
-	private String topicName;
-	
 	@Bean
 	@Scope("prototype")
 	public SimpleDateFormat dateFormat() {
 		return new SimpleDateFormat(Constantes.DATE_FORMAT);
 	}
-	
-	/**
-	 * @return
-	 */
-	@Bean
-    public Queue queue() {
-        return new Queue(queueName, false);
-    }
-	
-	@Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(topicName);
-    }
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(queueName);
-    }
-    
-    @Bean
-    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
-        return rabbitTemplate;
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
 }
---
+++ src/main/java/net/bounceme/chronos/comunicacion/config/RabbitConfig.java
@@ -0,0 +1,52 @@
+package net.bounceme.chronos.comunicacion.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+	
+	@Value("${application.queue}")
+	private String queueName;
+	
+	@Value("${application.topic}")
+	private String topicName;
+	
+	/**
+	 * @return
+	 */
+	@Bean
+    public Queue queue() {
+        return new Queue(queueName, false);
+    }
+	
+	@Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(topicName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+    
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
